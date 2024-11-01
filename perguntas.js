d7cc7de29de60555f22bb74a0b3f12de1ca27a70
Batata Doce <167936124+BatataDoce90@users.noreply.github.com>
2024-11-01 14:45:46 -0300

    Yeah

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital de São Paulo?',
+    'Exatamente, São Paulo'
+)
+
+criaCartao(
+    'Física',
+    'Quais são as leis de Newton?',
+    'São:  Lei da Inércia, Lei da Dinâmica e Lei da Ação e Reação'
+)
+
+criaCartao(
+    'Fatos',
+    'Qual a melhor professora do mundo?',
+    'Obviamente a Professora Luana Beatriz, que ensina os alunos da melhor forma possível, tornando ela, facilmente, a melhor professora de todas!'
+)
